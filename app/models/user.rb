b9472fb781dec1c6af1c9815f32c1d276817b523
yonakahara <peter20000118@gmail.com>
2021-04-18 15:41:06 +0900

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    # 仮想の属性を作成（dbに保存されない）
+    attr_accessor :remember_token
     before_save {self.email = email.downcase}
     validates(:name, presence: true, length: { maximum: 50 })
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -14,6 +16,27 @@ class User < ApplicationRecord
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+
+    # ランダムなトークンを返す
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        # update_attributeを使って、validationをスキップして属性を更新する
+        # remember_digest属性にdigestされたremember_tokenを格納する
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+        update_attribute(:remember_digest, nil)
     end
 
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,4 +17,18 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
+  def log_in_as(user)
+    session[:user_id] = uesr_id
+  end
+
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする。sessionメソッド使えないのでpostで実装。
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
